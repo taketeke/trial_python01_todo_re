47b32cc9503c8ae77098dd443a2e2bfd947f262a
taketeke <takeshi@sophie.localdomain>
2018-11-11 19:55:41 +0900

    2018/11/11 各機能ごとにメソッドを作り、Model,formsと紐づける

---
+++ mysite/todo/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    message = models.CharField(
+        max_length=100,
+        verbose_name="タスク",
+    )
+    created_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="登録日時",
+    )
